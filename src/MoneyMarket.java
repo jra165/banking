d10eb64ddc6c99198e58f33961c6c2f3ba44ea8b
kyle-t-lee <36808274+kyle-t-lee@users.noreply.github.com>
2020-10-03 23:05:54 -0400

    Set Up Classes and Required Methods/Variables

--- src/MoneyMarket.java
+++ src/MoneyMarket.java
@@ -1,4 +1,16 @@
 
-public class MoneyMarket {
+public class MoneyMarket extends Account {
+
+	@Override
+	public double monthlyInterest() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double monthlyFee() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
