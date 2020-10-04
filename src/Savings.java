0929f41dd45322805a753a66e1ac669ed52123f1
kyle-t-lee <36808274+kyle-t-lee@users.noreply.github.com>
2020-10-04 00:39:16 -0400

    Populated Subclasses of Account, Account, and AccountDatabase

--- src/Savings.java
+++ src/Savings.java
@@ -1,18 +1,48 @@
 
 public class Savings extends Account {
 
+	public Savings(Profile holder, double balance, Date dateOpen) {
+		super(holder, balance, dateOpen);
+		// TODO Auto-generated constructor stub
+	}
+
 	private boolean isLoyal;
 	
 	@Override
 	public double monthlyInterest() {
-		// TODO Auto-generated method stub
-		return 0;
+		final double annualInterestRate = 0.0005;
+		int period = 12;
+		final double monthlyInterestRate = annualInterestRate/period;
+		final double loyalInterestRate = 0.0035;
+		double monthlyInterest;
+		
+		if (isLoyal) {
+			monthlyInterest = getBalance() * loyalInterestRate; 
+		}
+		else {
+			monthlyInterest = getBalance() * monthlyInterestRate;
+		}
+		
+		
+		return monthlyInterest;
 	}
 
 	@Override
 	public double monthlyFee() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		double monthlyFee;
+		final double threshold = 300;
+		final double savingsMonthlyFee = 5;
+		
+		if(getBalance() >= threshold) {
+			monthlyFee = 0;
+		}
+		else {
+			monthlyFee = savingsMonthlyFee;
+		}
+
+		return monthlyFee;
+		
 	}
 
 }
