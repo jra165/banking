166fffd95fd28ab712f09cfdf94208f3605dbb61
jra165 <36318088+jra165@users.noreply.github.com>
2020-10-07 21:07:56 -0400

    try catch

--- src/ShitManager.java
+++ src/ShitManager.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
@@ -65,255 +66,263 @@ public class ShitManager {
 
 			while(s.hasNext()) {
 
-				com = s.next();
-				
-				//command only has one token if it is quit or print
-				if(is_one_token) {
+				try {
+					
+					com = s.next();
+					
+					//command only has one token if it is quit or print
+					if(is_one_token) {
 
-					if(com.equals("PA")) {
-						db.printAccounts();
-					}
-					 
-					//needs to calculate monthly interest and fees
-					//needs to print interest, fee, new balance depending on type of account
-					else if(com.equals("PD")) {
-						db.printByDateOpen();
-					}
-					 
-					//needs to print interest, fee, new balance depending on type of account
-					else if(com.equals("PN")) {
-						db.printByLastName();
-					}
-					
-					else if(com.equals("Q")) {
-						System.out.println("Transaction processing completed.");
-						break;
-					}
-					
-					else {
-						System.out.println("Command '" + command + "' not supported!");
-						break;
-					}
-					
-				}
-				
-				//command with three tokens is close
-				else if(is_three_tokens) {
-					
-					if(com.equals("CC") || com.equals("CS") || com.equals("CM")) {
+						if(com.equals("PA")) {
+							db.printAccounts();
+						}
 						 
-						fName = s.next();
-						lName = s.next();
-						Profile profile = new Profile(fName, lName);
-						boolean closed = false;
+						//needs to calculate monthly interest and fees
+						//needs to print interest, fee, new balance depending on type of account
+						else if(com.equals("PD")) {
+							db.printByDateOpen();
+						}
+						 
+						//needs to print interest, fee, new balance depending on type of account
+						else if(com.equals("PN")) {
+							db.printByLastName();
+						}
 						
-						if(com.equals("CC")) {
-							 
-							// Create a new Checking Account based on profile (invoke constructor 2)
-							Account currCheckAcc = new Checking(profile);
-							closed = db.remove(currCheckAcc);		// remove that account from the array
-							 
+						else if(com.equals("Q")) {
+							System.out.println("Transaction processing completed.");
+							break;
 						}
-						 
-						else if(com.equals("CS")) {
-							 
-							// Create a new Savings Account based on profile (invoke constructor 2)
-							Account currSavingsAcc = new Savings(profile);
-							closed = db.remove(currSavingsAcc);		// remove that account from the array
-						}
-						 
-						else if(com.equals("CM")) {
-							 
-							// Create a new Money Market Account based on profile (invoke constructor 2)
-							Account currMoneyMarketAcc = new Savings(profile);
-							closed = db.remove(currMoneyMarketAcc);	// remove that account from the array
-						}
-						 
-						if (closed == true) {
-							System.out.println("Account closed and removed from the database");
-						}
-						else {
-							System.out.println("Account does not exist");
-						}
-						  
-					}
-					
-					else {
-						System.out.println("Command '" + command + "' not supported!");
-						break;
-					}
-					
-				}
-				
-				//command with four tokens is deposit and withdrawal
-				else if(is_four_tokens) {
-					
-					if(com.equals("DC") || com.equals("DS") || com.equals("DM")) {
-						 
-						fName = s.next();
-						lName = s.next();
-						balance = s.nextDouble();
-						Profile profile = new Profile(fName, lName);
-						boolean deposited = false;
-						 
-						if (com.equals("DC")) {
-							Account currCheckingAcc = new Checking(profile);
-							deposited = db.deposit(currCheckingAcc, balance);
-						}
-						 
-						else if (com.equals("DS")) {
-							Account currSavingsAcc = new Savings(profile);
-							deposited = db.deposit(currSavingsAcc, balance);
-						}
-						 
-						else if (com.equals("DM")) {
-							Account currMoneyMarketAcc = new MoneyMarket(profile);
-							deposited = db.deposit(currMoneyMarketAcc, balance);
-						}
-						 
+						
 						else {
 							System.out.println("Command '" + command + "' not supported!");
 							break;
 						}
 						
-						if (deposited) {
-							System.out.println(balance + " deposited to account.");
-						}
-						else {
-							System.out.println("Account does not exist.");
-						}
-						 
 					}
 					
-					else if(com.equals("WC") || com.equals("WS") || com.equals("WM")) {
-						 
-						fName = s.next();
-						lName = s.next();
-						balance = s.nextDouble();
-						Profile profile = new Profile(fName, lName);
-						int withdrawn = 0;
-						 
-						 
-						if (com.equals("WC")) {
-							Account currCheckingAcc = new Checking(profile);
-							withdrawn = db.withdrawal(currCheckingAcc, balance);
-						}
-						 
-						else if (com.equals("WS")) {
-							Account currSavingsAcc = new Savings(profile);
-							withdrawn = db.withdrawal(currSavingsAcc, balance);
-						}
-						 
-						else if (com.equals("WM")) {
-							Account currMoneyMarketAcc = new MoneyMarket(profile);
-							withdrawn = db.withdrawal(currMoneyMarketAcc, balance);
-						}
-						 
-						if (withdrawn == 0) {
-							System.out.println(balance + " withdrawn from account.");
-						}
-						else if (withdrawn == 1) {
-							System.out.println("Insufficient funds.");
-						}
-						else {
-							System.out.println("Account does not exist.");
-						}
-						 
-					}
-					
-					else {
-						System.out.println("Command '" + command + "' not supported!");
-						break;
-					}
-					
-				}
-				
-				//command with five tokens is open money market account
-				else if(is_five_tokens) {
-					
-					if(com.equals("OM")) {
-
-						fName = s.next();
-						lName = s.next();
-						balance = s.nextInt();
-						date = s.next();
-
-						Profile profile = new Profile(fName, lName);
-						Date dateOpen = new Date(date);
-						boolean opened = false;
-
-						MoneyMarket moneymarket = new MoneyMarket(profile, balance, dateOpen);
-						opened = db.add(moneymarket);
+					//command with three tokens is close
+					else if(is_three_tokens) {
 						
-						if(opened == true) {
-							System.out.println("Account opened and added to the database");
-						}
-						
-						else {
-							System.out.println("Account is already in the database.");
-						}
+						if(com.equals("CC") || com.equals("CS") || com.equals("CM")) {
 							 
-					}
-					
-					else {
-						System.out.println("Command '" + command + "' not supported!");
-						break;
-					}
-					
-				}
-				
-				//command with six tokens is open checking/savings account
-				else if(is_six_tokens) {
-
-					if(com.equals("OC") || com.equals("OS")) {
-						
-						
-						fName = s.next();
-						lName = s.next();
-						balance = s.nextInt();
-						date = s.next();
-						special = s.nextBoolean();
-							 
-						Profile profile = new Profile(fName, lName);
-						Date dateOpen = new Date(date);
-						boolean opened = false;
-						
-						//magic numbers
-						if(com.equals("OC")) {
-							Checking checking = new Checking(profile, balance, dateOpen, special);
-							opened = db.add(checking);
-						} 
-							 
-						else if(com.equals("OS")) {
+							fName = s.next();
+							lName = s.next();
+							Profile profile = new Profile(fName, lName);
+							boolean closed = false;
+							
+							if(com.equals("CC")) {
 								 
-							Savings savings = new Savings(profile, balance, dateOpen, special);
-							opened = db.add(savings);
+								// Create a new Checking Account based on profile (invoke constructor 2)
+								Account currCheckAcc = new Checking(profile);
+								closed = db.remove(currCheckAcc);		// remove that account from the array
 								 
-						}
-					
-						
-						if(opened == true) {
-							System.out.println("Account opened and added to the database");
+							}
+							 
+							else if(com.equals("CS")) {
+								 
+								// Create a new Savings Account based on profile (invoke constructor 2)
+								Account currSavingsAcc = new Savings(profile);
+								closed = db.remove(currSavingsAcc);		// remove that account from the array
+							}
+							 
+							else if(com.equals("CM")) {
+								 
+								// Create a new Money Market Account based on profile (invoke constructor 2)
+								Account currMoneyMarketAcc = new Savings(profile);
+								closed = db.remove(currMoneyMarketAcc);	// remove that account from the array
+							}
+							 
+							if (closed == true) {
+								System.out.println("Account closed and removed from the database");
+							}
+							else {
+								System.out.println("Account does not exist");
+							}
+							  
 						}
 						
 						else {
-							System.out.println("Account is already in the database.");
+							System.out.println("Command '" + command + "' not supported!");
+							break;
 						}
-						 
+						
+					}
+					
+					//command with four tokens is deposit and withdrawal
+					else if(is_four_tokens) {
+						
+						if(com.equals("DC") || com.equals("DS") || com.equals("DM")) {
+							 
+							fName = s.next();
+							lName = s.next();
+							balance = s.nextDouble();
+							Profile profile = new Profile(fName, lName);
+							boolean deposited = false;
+							 
+							if (com.equals("DC")) {
+								Account currCheckingAcc = new Checking(profile);
+								deposited = db.deposit(currCheckingAcc, balance);
+							}
+							 
+							else if (com.equals("DS")) {
+								Account currSavingsAcc = new Savings(profile);
+								deposited = db.deposit(currSavingsAcc, balance);
+							}
+							 
+							else if (com.equals("DM")) {
+								Account currMoneyMarketAcc = new MoneyMarket(profile);
+								deposited = db.deposit(currMoneyMarketAcc, balance);
+							}
+							 
+							else {
+								System.out.println("Command '" + command + "' not supported!");
+								break;
+							}
+							
+							if (deposited) {
+								System.out.println(balance + " deposited to account.");
+							}
+							else {
+								System.out.println("Account does not exist.");
+							}
+							 
+						}
+						
+						else if(com.equals("WC") || com.equals("WS") || com.equals("WM")) {
+							 
+							fName = s.next();
+							lName = s.next();
+							balance = s.nextDouble();
+							Profile profile = new Profile(fName, lName);
+							int withdrawn = 0;
+							 
+							 
+							if (com.equals("WC")) {
+								Account currCheckingAcc = new Checking(profile);
+								withdrawn = db.withdrawal(currCheckingAcc, balance);
+							}
+							 
+							else if (com.equals("WS")) {
+								Account currSavingsAcc = new Savings(profile);
+								withdrawn = db.withdrawal(currSavingsAcc, balance);
+							}
+							 
+							else if (com.equals("WM")) {
+								Account currMoneyMarketAcc = new MoneyMarket(profile);
+								withdrawn = db.withdrawal(currMoneyMarketAcc, balance);
+							}
+							 
+							if (withdrawn == 0) {
+								System.out.println(balance + " withdrawn from account.");
+							}
+							else if (withdrawn == 1) {
+								System.out.println("Insufficient funds.");
+							}
+							else {
+								System.out.println("Account does not exist.");
+							}
+							 
+						}
+						
+						else {
+							System.out.println("Command '" + command + "' not supported!");
+							break;
+						}
+						
+					}
+					
+					//command with five tokens is open money market account
+					else if(is_five_tokens) {
+						
+						if(com.equals("OM")) {
+
+							fName = s.next();
+							lName = s.next();
+							balance = s.nextInt();
+							date = s.next();
+
+							Profile profile = new Profile(fName, lName);
+							Date dateOpen = new Date(date);
+							boolean opened = false;
+
+							MoneyMarket moneymarket = new MoneyMarket(profile, balance, dateOpen);
+							opened = db.add(moneymarket);
+							
+							if(opened == true) {
+								System.out.println("Account opened and added to the database");
+							}
+							
+							else {
+								System.out.println("Account is already in the database.");
+							}
+								 
+						}
+						
+						else {
+							System.out.println("Command '" + command + "' not supported!");
+							break;
+						}
+						
+					}
+					
+					//command with six tokens is open checking/savings account
+					else if(is_six_tokens) {
+
+						if(com.equals("OC") || com.equals("OS")) {
+							
+							
+							fName = s.next();
+							lName = s.next();
+							balance = s.nextInt();
+							date = s.next();
+							special = s.nextBoolean();
+								 
+							Profile profile = new Profile(fName, lName);
+							Date dateOpen = new Date(date);
+							boolean opened = false;
+							
+							//magic numbers
+							if(com.equals("OC")) {
+								Checking checking = new Checking(profile, balance, dateOpen, special);
+								opened = db.add(checking);
+							} 
+								 
+							else if(com.equals("OS")) {
+									 
+								Savings savings = new Savings(profile, balance, dateOpen, special);
+								opened = db.add(savings);
+									 
+							}
+						
+							
+							if(opened == true) {
+								System.out.println("Account opened and added to the database");
+							}
+							
+							else {
+								System.out.println("Account is already in the database.");
+							}
+							 
+						}
+						
+						else {
+							System.out.println("Command '" + command + "' not supported!");
+							break;
+						}
+
 					}
 					
 					else {
 						System.out.println("Command '" + command + "' not supported!");
 						break;
 					}
-
+					
+				} catch(InputMismatchException e) {
+					System.out.println("InputMismatchException.");
 				}
-				
-				else {
-					System.out.println("Command '" + command + "' not supported!");
-					break;
+				catch(NumberFormatException e) {
+					System.out.println("NumberFormatException");
 				}
-
 			}
 			
 			s.close();
